df4ef06133484646189a1f05a18b0bcbf430c2f4
Christoffer Winterkvist <chris@zenangst.com>
2014-10-09 11:27:24 +0200

    Set up HYPAccountNumberTests

--- HYPAccountNumberTests/HYPAccountNumberTests.m
+++ HYPAccountNumberTests/HYPAccountNumberTests.m
@@ -8,8 +8,13 @@
 
 #import <Cocoa/Cocoa.h>
 #import <XCTest/XCTest.h>
+#import "HYPNorwegianAccountNumber.h"
+
+static NSString * HYPAccountNumberSubject = @"11111111111";
 
 @interface HYPAccountNumberTests : XCTestCase
+
+@property (nonatomic, strong) HYPNorwegianAccountNumber *accountNumber;
 
 @end
 
@@ -17,24 +22,14 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.accountNumber = [[HYPNorwegianAccountNumber alloc] initWithString:HYPAccountNumberSubject];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
